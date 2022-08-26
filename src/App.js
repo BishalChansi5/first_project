a593917a58f04557f1e128be8b45e67f5c64a893
Bishal Chansi <chansibishal55@gmail.com>
2022-08-26 12:53:05 +0545

    Component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       <p> Hello, this is me..</p>
+      <Hello place="Bishal"/>
     </div>
   );
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hello.css';
+
+const Hello = ({place}) => {
+  return (
+    <p>Hello this is me {place}.</p>
+  )
+}
+
+export default Hello
+
