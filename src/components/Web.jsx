03f5c69236f8f2b96f454c4207911a8d3ae96c6b
Bishal Chansi <chansibishal55@gmail.com>
2022-09-01 11:10:33 +0545

    loop

--- src/components/Web.jsx
+++ src/components/Web.jsx
@@ -9,15 +9,35 @@ const Web = () => {
     "Today is thursday"
   ])
 
+  console.log (inputData);
+function handleSubmit(){
+  setTodos([...todos,inputData])
+  setInputData(' ')
+}
+function handleRemove(){
+  setTodos([ ])
+}
   return (
     <div className='Web'>
         <div className='logo'>
         <h3>Todo APP</h3>
         </div>
         <div className='form'>
-            <input type="text" placeholder='Enter the items'/>
-            <input type="button" value="Add Todo"/>
+            <input
+                 type="text" 
+                 placeholder='Enter the items' 
+                 value={inputData} 
+                 onChange={(e)=> setInputData (e.target.value)}
+            />
+            <input 
+            type="button" 
+            value="Add Todo"
+            onClick={handleSubmit}
+            />
+
         </div>
+
+
         <div className='lists'>
            <h3>Todo Lists</h3>
            <ul>
@@ -31,7 +51,7 @@ const Web = () => {
            </ul>
         </div>
         <div className='remove'>
-            <button>remove app</button>
+            <button onClick={handleRemove}>remove app</button>
         </div>
     </div>
   )
